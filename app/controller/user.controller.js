33cf5ed4e5dab89988571432c24c0d16bb9fd59c
Vishalbhilavala <vishalbhilavala.shivinfotech@gmail.com>
2025-03-06 23:42:29 +0530

    initial Set-up complete

---
+++ app/controller/user.controller.js
@@ -0,0 +1,11 @@
+const db = require('../database/db')
+const logger = require('../services/logger');
+const {GeneralError, BadRequest} = require('../utils/error');
+const {response} = require('../utils/enum');
+const {StatusCodes} = require('http-status-codes');
+const message = require('../utils/message');
+const {GeneralResponse} = require('../utils/responce');
+
+module.exports = {
+    
+}
---
+++ app/database/db.js
@@ -0,0 +1,33 @@
+const Sequelize = require('sequelize');
+const logger = require('../services/logger')
+require('dotenv').config()
+
+const sequelize = new Sequelize(
+    'sequelize-boiler-plate',
+    'root',
+    'RAMJIBHAI@BHILAVALA78',
+    {
+        dialect: 'mysql',
+        host: 'localhost',
+        port: 3306,
+        logging: false
+    }
+);
+
+sequelize.authenticate().then(()=>{
+    logger.info('Connection has been established successfully.')
+}).catch((error) =>{
+    logger.error('Unable to connect to the database: ', error)
+})
+
+const db = {};
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+
+db.sequelize.sync({force: false}).then(() =>{
+    logger.info('Database synchronized successfully.')
+}).catch((error) => {
+    logger.error('Error synchronizing database:', error);
+});
+
+module.exports = db;
