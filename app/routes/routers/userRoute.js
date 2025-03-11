e73aee493518ed6da66b621bfff17dc5d627e172
Jenish <jenishbhilavala.shivinfotech@gmail.com>
2025-03-11 16:07:48 +0530

    Revert "initial Set-up complete"

--- app/routes/routers/userRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const route = express.Router()
-const userModel = require('../../controller/user.controller')
-
-route.post('/createUser',)
-
-
-module.exports = route
